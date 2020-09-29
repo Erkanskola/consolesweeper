01ed9e8e2c9a5cc88507601ce61f91ef05c14d06
Erik Calissendorff <erik.calissendorff@learnet.se>
2020-09-29 12:27:46 +0200

    initial commit

---
+++ consolesweeper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace consolesweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int y = 100;
+
+            DoThing(ref y);
+
+            System.Console.WriteLine(y);
+
+
+            Console.ReadLine();
+
+
+        }
+
+        static void DoThing(ref int k)
+        {
+            k++;
+        }
+    }
+}
