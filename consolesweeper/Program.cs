2af0a6e0b8572afb0a6eb443c2cd9e91f9b6fe94
Erik Calissendorff <erik.calissendorff@learnet.se>
2020-10-08 16:08:09 +0200

    försöker göra gränssnittet

--- consolesweeper/Program.cs
+++ consolesweeper/Program.cs
@@ -13,9 +13,28 @@ namespace consolesweeper
             sweeperGrid.CheckPosition(10,10);
 
             System.Console.WriteLine("Skriv in ett X-värde");
-            System.Console.WriteLine("Skriv in ett Y-värde");
+
+
+
+
+            string xKo = Console.ReadLine();
+            string yKo = Console.ReadLine();
+
+           bool success = false;
+    
+            while(!success)
+            {
+                success = int.TryParse(xKo, out int xK);
+                if (xK <= 10 || xK >= 1)
+                {
+                    System.Console.WriteLine($"Din X-koordinat är {xK}");
+                }
+                break;
+            }
+
 
             Console.ReadLine();
+
 
 
         
